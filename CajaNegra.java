d250823e6b1b78cad5f790ca70e5a379d06fcc1c
VictoriaDiaz <daw102.2026@gmail.com>
2026-02-16 12:36:47 +0100

    Comentado

--- CajaNegra.java
+++ CajaNegra.java
@@ -1,8 +1,22 @@
 package repaso;
 
+/**
+ * 
+ * @param
+ * @author DAW102
+ * @version 1.10.1
+ */
 public class CajaNegra {
 
+	/**
+	 * Método que comprueba el DNI
+	 * @param nif indica el numero del DNI
+	 * @param letra indica la letra del DNI
+	 * @return true o false
+	 */
+	
 	public boolean compruebaDNI(int nif, char letra) {
+		//el parámetro NIF tiene que ser mayor a 10000000 y menor que 100000000
 		if (nif <= 10000000 && nif >= 100000000) {
 			return false;
 		}
